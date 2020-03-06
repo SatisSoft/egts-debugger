b9da9b06bbf748e3a5873a3f49eb5d97bc453260
ashirko <antonshirko@frtk.ru>
2020-03-06 09:16:29 +0300

    Fix rnis_connector

--- rnis_connector.py
+++ rnis_connector.py
@@ -1,6 +1,6 @@
 import logging
 import crcmod
-from egts import *
+from egtsdebugger.egts import *
 import socket
 
 auth_packet = b"\x01\x00\x00\x0b\x00\x0f\x00\x01\x00\x01\x06\x08\x00\x01\x00\x38\x01\x01\x05\x05\x00\x00\xef" \
